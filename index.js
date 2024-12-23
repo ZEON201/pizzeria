2a4a6285ca86936a262eaac21064fb15f28febba
Igor <cool.ivanovigor2001@ya.ru>
2024-12-23 17:28:54 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Happy developing ✨')
+const express = require('express')
+let cors = require('cors')
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+module.exports = app;
+
